c83c7d2a933cba499919315c9d9a651a24c84f84
ktny <katsunori.t10@gmail.com>
2023-03-11 17:16:36 +0900

    promptクオリティ向上

--- app/entry.py
+++ app/entry.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import json
+import random
 import sys
+from typing import Any, Dict
 
 import openai
 import requests
-from config import OPENAI_API_KEY
+from config import OPENAI_API_KEY, gpt_system_message
 from session import create_hatena_session
 
 read_entry_endpoint = "https://b.hatena.ne.jp/entry/jsonlite/"
@@ -14,46 +16,80 @@ bookmark_entry_endpoint = "https://bookmark.hatenaapis.com/rest/1/my/bookmark"
 openai.api_key = OPENAI_API_KEY
 
 
-def read_entry(url):
-    # APIにリクエストを送信
+def read_entry(url: str):
     response = requests.get(read_entry_endpoint + url)
     return json.loads(response.text)
 
 
-def bookmark_entry(session, url, comment):
-    # ブックマークを追加するためのデータ
-    params = {
-        "url": url,
-        "comment": comment,
-        # "tags": "example, bookmark",
-    }
-
-    # APIにリクエストを送信
-    return session.post(bookmark_entry_endpoint, params=params)
+def bookmark_entry(session: Any, url: str, comment: str):
+    return session.post(
+        bookmark_entry_endpoint,
+        params={"url": url, "comment": comment, "post_twitter": True},
+    )
 
 
-def generate_comment(entry):
+def generate_comment(entry: Dict):
     prompt = generate_prompt(entry)
-    print(prompt)
-
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "あなたはお嬢様です"},
+            {"role": "system", "content": gpt_system_message},
             {"role": "user", "content": prompt},
         ],
         temperature=0.9,
     )
+
+    # print(response)
+
     return response["choices"][0]["message"]["content"]
 
 
-def generate_prompt(entry):
-    return f"""次の記事のタイトルと記事に対するコメントから感想をコメントしてください
+def generate_prompt(entry: Dict):
+    bookmarks = [
+        bookmark["comment"]
+        for bookmark in entry["bookmarks"]
+        if bookmark.get("comment")
+    ]
 
-制約: 60文字以内
-記事のタイトル：{entry["title"]}
-記事に対するコメント：うーむ
+    if len(bookmarks) > 30:
+        bookmarks = random.sample(bookmarks, 30)
+    comments = ",".join(bookmarks)
+
+    return f"""次のお題に対して「一番星はての」としてコメントしてください。
+
+お題
+
+・タイトル：{entry["title"]}
+・ブコメ：{comments}
+
+以下の制約を守ってコメントしてください。
+
+・100文字以内で簡潔にコメントする
+・お題は「タイトル」を60%、「ブコメ」を40%の割合でどちらも参考にコメントする
+・均等な確率で「楽観的」「可能性」「話題の変更」「同意」「質問」「提案」のいずれかのコメントをする
+・「悲観的」「問題点の指摘」となるようなコメントはしない
+
+以下の形式でコメントしてください。
+
+一番星はての：コメント
 """
+
+
+def fix_comment(comment: str):
+    excluding_words = ["（楽観的）", "（可能性）", "（話題の変更）", "（同意）", "（質問）", "（提案）"]
+    for word in excluding_words:
+        comment = comment.replace(word, "")
+
+    comment = comment.replace("一番星はての：", "").strip()
+    sentences = comment.strip("。").split("。")
+    result = ""
+    for sentence in sentences:
+        if len(result + sentence + "。") <= 100:
+            result += sentence + "。"
+        else:
+            break
+
+    return result
 
 
 if __name__ == "__main__":
@@ -64,10 +100,10 @@ if __name__ == "__main__":
 
     session = create_hatena_session()
     entry = read_entry(url)
-    comment = generate_comment(entry)
+    comment = fix_comment(generate_comment(entry))
+
     print(comment)
 
-    res = bookmark_entry(session, entry["url"], comment)
+    res = bookmark_entry(session, url, comment)
 
-    print(res.status_code)
     print(res.text)
