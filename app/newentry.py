2534c15e7600477b7dbda45e5fca5637e32995d4
ktny <katsunori.t10@gmail.com>
2023-03-27 16:39:27 +0900

    add newentry

---
+++ app/newentry.py
@@ -0,0 +1,48 @@
+import random
+
+import requests
+from bs4 import BeautifulSoup
+from entry import bookmark_by_gpt
+from util.models import Entry
+
+COMMENT_ARTICLE_COUNT = 1
+
+# ホットエントリページの取得、解析
+res = requests.get("http://b.hatena.ne.jp/hotentry")
+soup = BeautifulSoup(res.text, "html.parser")
+
+hotentries = []
+entries = soup.select(".entrylist-main .entrylist-contents-main")
+
+
+for entry in entries:
+    title_link = entry.find("a")
+    title = title_link.get("title")
+    url = title_link.get("href")
+    category = title_link.get("data-entry-category")
+
+    # はてのが言及された記事は優先的にブックマークする
+    if "一番星はての" in title:
+        bookmark_by_gpt(url)
+        continue
+
+    # はてのは政治経済とテクノロジーには基本的に疎い
+    if category in ["政治と経済", "テクノロジー"]:
+        continue
+
+    hotentries.append(Entry(url, title, category))
+
+
+count = 0
+entried_categories = []
+random.shuffle(hotentries)
+
+for i, entry in enumerate(hotentries, start=1):
+    print("####################################################")
+    print(f"{i}: [{entry.category}]{entry.title}({entry.url})\n")
+    success = bookmark_by_gpt(entry.url)
+    if success:
+        count += 1
+    if count >= COMMENT_ARTICLE_COUNT:
+        print("break")
+        break
