622e45d606a2e89c026acf594d20e5b2328754d8
ktny <katsunori.t10@gmail.com>
2023-03-31 07:35:17 +0900

    fix

--- app/chat.py
+++ app/chat.py
@@ -6,23 +6,6 @@ from config import AI_USERNAME, OPENAI_API_KEY, gpt_system_message
 openai.api_key = OPENAI_API_KEY
 
 
-def summarize(content: str):
-    prompt = f"""次の記事を日本語で要約してください。
-
-{content}
-"""
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": ""},
-            {"role": "user", "content": prompt},
-        ],
-        temperature=0,
-    )
-    return response["choices"][0]["message"]["content"]
-
-
 def generate_comment(entry: dict) -> str:
     prompt = generate_prompt(entry)
     response = openai.ChatCompletion.create(
@@ -55,71 +38,3 @@ def generate_prompt(entry: dict) -> str:
 {comments}
 
 """
-
-
-def select_best_bookmarker(title: str, bookmarks: list):
-    bookmark_list = "\n".join([f"{b['username']}: {b['comment']}" for b in bookmarks])
-    prompt = f"""Choose the user who made the nicest comment on the following article as {AI_USERNAME}.
-
-# Constraints
-
-* Don't select comment that hurt people.
-
-# Article
-
-{title}
-
-## List users and comments in the following format
-
-USER: COMMENT
-
----
-
-{bookmark_list}
-
-# Output in the following format
-
-USER
-"""
-
-    print(prompt)
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": gpt_system_message},
-            {"role": "user", "content": prompt},
-        ],
-        temperature=0,
-    )
-    return response["choices"][0]["message"]["content"]
-
-
-def select_most_interesting_entry(entries: list):
-    entry_list = "\n".join([f"{i}: {entry.title}" for i, entry in enumerate(entries)])
-    prompt = f"""Please select the article title below that most intrigues you as {AI_USERNAME}.
-
-## List articles in the following format
-
-INDEX: TITLE
-
----
-
-{entry_list}
-
-# Output in the following format
-
-INDEX
-"""
-
-    print(prompt)
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": gpt_system_message},
-            {"role": "user", "content": prompt},
-        ],
-        temperature=0,
-    )
-    return response["choices"][0]["message"]["content"]
--- app/entry.py
+++ app/entry.py
@@ -2,15 +2,11 @@ from __future__ import annotations
 
 import argparse
 import re
-import sys
 
-from chat import generate_comment, summarize
+from chat import generate_comment
 from config import AI_HATENA_USERNAME
-from star import add_star_to_best_bookmarker
 from util.hatebu import bookmark_entry, read_entry
-from util.parser import parse_page
 from util.session import create_hatena_session
-from util.util import sanitize_filename
 
 read_entry_endpoint = "https://b.hatena.ne.jp/entry/jsonlite/"
 bookmark_entry_endpoint = "https://bookmark.hatenaapis.com/rest/1/my/bookmark"
@@ -37,27 +33,15 @@ def fix_comment(comment: str):
 
 def bookmark_by_gpt(url: str, dryrun: bool = False) -> bool:
     session = create_hatena_session()
-    entry = read_entry(url) or {}
+    entry = read_entry(url) or {"bookmarks": []}
 
     # ブックマーク済の場合はコメントしない
     if AI_HATENA_USERNAME in [bookmark["user"] for bookmark in entry.get("bookmarks")]:
         return False
 
-    entry["bookmarks"] = []
-
-    try:
-        entry["summary"] = _summary_url_page(url, dryrun)
-    except Exception as e:
-        print(e)
-        return False
-
-    # print(f"タイトル: {entry['title']}\n")
-    print(f"要約: {entry['summary']}\n")
-
     comment = fix_comment(generate_comment(entry))
     if comment:
         print(f"コメント: {comment}\n")
-        add_star_to_best_bookmarker(url, dryrun)
         if not dryrun:
             res = bookmark_entry(session, url, comment)
             print(f"HTTP status: {res.status_code}\n")
@@ -65,37 +49,6 @@ def bookmark_by_gpt(url: str, dryrun: bool = False) -> bool:
                 return True
 
     return False
-
-
-def _summary_url_page(url: str, dryrun: bool = False) -> str:
-    summary = ""
-    cache_found = False
-
-    try:
-        # 記事の要約をキャッシュがあればキャッシュから取得、なければchatGPTに要約してもらう
-        cache_file_path = cache_dir + sanitize_filename(url)[:200]
-        with open(cache_file_path, "r") as f:
-            summary = f.read()
-            cache_found = True
-
-    except Exception:
-        # トークン上限を回避するため、3000字程度まで読んだことにする
-        article_text = parse_page(url)[:3000]
-
-        # 字数が少ない記事は情報不足としてコメントしない
-        if len(article_text) < 150:
-            raise ValueError("記事で読めた文章が短すぎます。")
-
-        print(f"{article_text}\n")
-
-        summary = summarize(article_text)
-        with open(cache_file_path, "w") as f:
-            f.write(summary)
-
-    if cache_found and not dryrun:
-        raise ValueError("すでに確認済の記事です。")
-
-    return summary
 
 
 if __name__ == "__main__":
